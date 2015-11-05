3609f704e25d34037172ea07b59c31e40714519f
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2015-11-06 00:27:19 +0200

    WED doc

--- docs/conf.py
+++ docs/conf.py
@@ -138,6 +138,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'css/wide.css'
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
@@ -172,10 +173,10 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
--- docs/index.rst
+++ docs/index.rst
@@ -13,6 +13,7 @@ Contents:
 
    general
    installation
+   yeti-ui
    ldap
    yeti-cli
 
