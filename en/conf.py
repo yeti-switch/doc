39dfc403ca148f964ad22ef89f10750445db866d
Dmitry <dmitry.sinina@onat.edu.ua>
2024-08-28 15:40:24 +0300

    drop sphinx_rtd_langs_theme

--- en/conf.py
+++ en/conf.py
@@ -108,7 +108,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_langs_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -117,7 +117,7 @@ html_theme = "sphinx_rtd_langs_theme"
 # }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../themes"]
+#html_theme_path = ["../themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
